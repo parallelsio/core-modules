4c34cab331ec4e9fae8a2b46336e4fae08d392d6
Steven J. Dale <lifeinchords@gmail.com>
2014-09-26 19:57:12 -0400

    feat: menu bounces in. w00t.

--- client/views/menu/menu.js
+++ client/views/menu/menu.js
@@ -16,7 +16,7 @@ Template.menu.rendered = function() {
   console.log('menu rendered.');
 
   var menuBar = document.getElementById("menu-bar");
-  TweenLite.to(menuBar, 1, { left:"632px" });
+  TweenLite.to(menuBar, 1, { top:"0px", ease:Elastic.easeOut});
 
 
 };
