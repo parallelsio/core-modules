12738f5abe2edd15d289f11929c798c8f831c4d0
Steven J. Dale <lifeinchords@gmail.com>
2015-04-06 17:53:45 -0400

    bit:image:preview: refactored, made more DRY, added clearer comments. Fixed a bug where the original thumbnail height/width wasnt set and used properly.

--- meteor-app/client/views/application/application.js
+++ meteor-app/client/views/application/application.js
@@ -5,15 +5,32 @@
 _ = lodash;
 
 
-  
+
 
 // TODO: refactor this out into its own namespace
 var scaleImage = function(bitData, bitPreviewingId, $bit, bitTemplate, direction){
 
   $bitImg = $(bitTemplate.templateInstance().$('img'));
 
-  var bitThumbnailHeight = $bitImg.height()
-  var bitThumbnailWidth = $bitImg.width()
+  var bitThumbnailHeight = $bitImg.height();
+  var bitThumbnailWidth = $bitImg.width();
+
+  var maskLeft         = $('.wipe.bit-preview.side-to-side .mask.left');
+  var maskRight        = $('.wipe.bit-preview.side-to-side .mask.right');
+
+  var documentWidth  = $(document).width();
+  var documentHeight = $(document).height();
+
+
+  var toWidth, toHeight, easeType;
+
+  // set up and calculate the vars to be used during the animation differently, 
+  // depending on if the image is to be expanded or contracted.
+
+  // when expanding, we try to make the image use up most of the viewport space to preview
+  // and save the image's thumbnail height+width in a session var
+  if (direction === "expand")
+  {
 
     // padding for top and bottom, in pixels
     var edgePadding = 10; 
@@ -41,141 +58,128 @@ var scaleImage = function(bitData, bitPreviewingId, $bit, bitTemplate, direction
            nativeWidth       x (previewWidth) 
       */
       var previewWidth = Math.floor((bitData.nativeWidth * previewHeight) / bitData.nativeHeight);
-      var toWidth, toHeight, easeType;
 
-      // *************** set the options, for what will happen when animation runs
-      // the main difference is that on the way down it bounces, to give it a feel like gravity is at play
-      if (direction === "expand")
-      {
-        toWidth = previewWidth;
-        toHeight = previewHeight;
-        easeType = Expo.easeOut;
-      }
-      
-      else if (direction === "contract")
-      {
-        toWidth = Session.get('bitThumbnailWidth');
-        toHeight = Session.get('bitThumbnailHeight');
-        easeType = Elastic.easeOut.config(2, 0.4)
-      }
-      
-      var options = { 
-        width: toWidth, 
-        height: toHeight,
-        scale: 1,
-        ease: easeType
-      }; 
+      toWidth = previewWidth;
+      toHeight = previewHeight;
+      easeType = Expo.easeOut;
 
-      var maskLeft         = $('.wipe.bit-preview.side-to-side .mask.left');
-      var maskRight        = $('.wipe.bit-preview.side-to-side .mask.right');
-    
-      var documentWidth  = $(document).width();
-      var documentHeight = $(document).height();
+    }
+  }
 
-      var timelineStart = function () {
-          console.log('bit:preview timeline starting ...');
-
-          // TODO: kill all running animations
-
-          // disable scrolling
-          $("body").css( "overflow", "hidden");
-
-          // TODO: disable bit actions (drag, delete)
-
-          if (direction === "expand")
-          {
-            Session.set('bitThumbnailHeight', bitThumbnailHeight);
-            Session.set('bitThumbnailWidth', bitThumbnailWidth);
-          }
-      };
-
-      var timelineDone = function( bitPreviewingId, direction ){
-        
-        if (direction === "expand")
-        {
-          Session.set('bitPreviewingId', bitPreviewingId);
-        }
-
-        else if (direction === "contract")
-        {
-          bitPreviewingId = "";
-          Session.set('bitPreviewingId', null);
-          Session.set('bitThumbnailWidth', null);
-          Session.set('bitThumbnailHeight', null);
-        }
-
-        console.log("bit:preview:", bitPreviewingId, "tween done." );
-      };
-
-      var timeline = new TimelineMax({ 
-        onStart: timelineStart,
-        onComplete: timelineDone, 
-        onCompleteParams:[ bitPreviewingId, direction ]
-      });
+  // when contracting (on the way down), the animation bounces to give it a feel 
+  // like gravity is at play. We use the original thumbnail height + width, 
+  // as our destination height + width, saved to the session when we expanded,
+  // and clear these vars when finished
+  else if (direction === "contract")
+  {
+    toWidth = Session.get('bitThumbnailWidth');
+    toHeight = Session.get('bitThumbnailHeight');
+    easeType = Elastic.easeOut.config(2, 0.4)
+  }
+  
+  var options = { 
+    width: toWidth, 
+    height: toHeight,
+    scale: 1,
+    ease: easeType
+  }; 
 
 
-      // zelda wipe in of overlay bg
-      // inspired by: https://www.youtube.com/watch?v=wHaZrYX0kAU&t=14m54s
-      if (direction === "expand")
-      {
-        console.log ("expanding");
+  var timelineStart = function () {
+    console.log('bit:preview timeline starting ...');
 
-        timeline
-          .set($('.wipe.bit-preview.side-to-side'), { alpha: 1, display: "block" })
-          .fromTo(maskRight,  0.25, { x:  documentWidth / 2, ease: Expo.easeOut }, { x: 0 }, 0.12 )
-          .fromTo(maskLeft,   0.25, { x: -documentWidth / 2, ease: Expo.easeOut }, { x: 0 }, 0.12 )
+    // TODO: kill all running animations
 
-          // TODO: this wont be needed after on hover, bit swaps to top of 
-          // z-index stack
-          .set($($bit), { zIndex: 10 })
+    // disable scrolling
+    $("body").css( "overflow", "hidden");
 
-          // TODO: move/center viewport around the image
+    // TODO: disable bit actions (drag, delete)
 
-          // blow up image from thumbnail size up to fit the viewport height
-          // TODO: overlap with zelda swipe in
-          .to($bitImg, 0.10, { scale: 0.9, ease:Quint.easeOut } )
-          .to($bitImg, 0.25, options );
-      }
-
-      else if (direction === "contract")
-      {
-        console.log ("contracting");
-
-        timeline
-
-          .fromTo(maskLeft, 0.25, { x: 0 }, { x: -documentWidth / 2, ease: Expo.easeOut }, 0.12 )
-          .fromTo(maskRight, 0.25, { x: 0 }, { x:  documentWidth / 2, ease: Expo.easeOut }, 0.12 )
-
-          // contract image from viewport height down to thumbnail size 
-          .to($bitImg, 0.10, { scale: 1.1, ease:Quint.easeOut } )
-          .to($bitImg, 0.25, options )
-
-          .set($($bit), { zIndex: 1 })
-          .set($('.wipe.bit-preview.side-to-side'), { alpha: 0, display: "none" });
-
-      }
-      
+    if (direction === "expand")
+    {
+      Session.set('bitThumbnailHeight', bitThumbnailHeight);
+      Session.set('bitThumbnailWidth', bitThumbnailWidth);
     }
   };
 
+  var timelineDone = function( bitPreviewingId, direction ){
+    
+    if (direction === "expand")
+    {
+      Session.set('bitPreviewingId', bitPreviewingId);
+    }
 
-Meteor.startup(function(){
+    else if (direction === "contract")
+    {
+      bitPreviewingId = "";
+      Session.set('bitPreviewingId', null);
+      Session.set('bitThumbnailWidth', null);
+      Session.set('bitThumbnailHeight', null);
+    }
 
+    console.log("bit:preview:", bitPreviewingId, "tween done." );
+  };
 
-  // TODO: move to another file
-  var utility = ({
-
-    // create utility
-    // var element = document.querySelector("[data-id='" + this.data._id + "']");
-    // var getParallelsID = ""
+  var timeline = new TimelineMax({ 
+    onStart: timelineStart,
+    onComplete: timelineDone, 
+    onCompleteParams:[ bitPreviewingId, direction ]
   });
 
+  // run the animations.
+  // the settings and sequencing is inspired by the Zelda 'wipes' 
+  // https://www.youtube.com/watch?v=wHaZrYX0kAU&t=14m54s
+  if (direction === "expand")
+  {
+    console.log("expanding...");
+
+    timeline
+      .set($('.wipe.bit-preview.side-to-side'), { alpha: 1, display: "block" })
+      .fromTo(maskRight,  0.25, { x:  documentWidth / 2, ease: Expo.easeOut }, { x: 0 }, 0.12 )
+      .fromTo(maskLeft,   0.25, { x: -documentWidth / 2, ease: Expo.easeOut }, { x: 0 }, 0.12 )
+
+      // TODO: this wont be needed after on hover, bit swaps to top of 
+      // z-index stack
+      .set($($bit), { zIndex: 10 })
+
+      // TODO: move/center viewport around the image
+
+      // blow up image from thumbnail size up to fit the viewport height
+      .to($bitImg, 0.10, { scale: 0.9, ease:Quint.easeOut } )
+      .to($bitImg, 0.25, options );
+  }
+
+  else if (direction === "contract")
+  {
+    console.log("contracting...");
+
+    timeline
+
+      .fromTo(maskLeft, 0.25, { x: 0 }, { x: -documentWidth / 2, ease: Expo.easeOut }, 0.12 )
+      .fromTo(maskRight, 0.25, { x: 0 }, { x:  documentWidth / 2, ease: Expo.easeOut }, 0.12 )
+
+      // contract image from viewport height down to original thumbnail size 
+      .to($bitImg, 0.10, { scale: 1.1, ease:Quint.easeOut } )
+      .to($bitImg, 0.25, options )
+
+      .set($($bit), { zIndex: 1 })
+      .set($('.wipe.bit-preview.side-to-side'), { alpha: 0, display: "none" });
+  }
+};
+
+
+Meteor.startup(function(){
 
   console.log("Meteor.startup start.");
   
     // reset any leftover session vars from last load
   Session.set('bitPreviewingId', null);
+  Session.set('bitThumbnailWidth', null);
+  Session.set('bitThumbnailHeight', null);
+
+  Session.set('bitHoveringId', null);
   Session.set('bitEditingId', null);
+
   Session.set('isDrawingParallel', false);
 
   
