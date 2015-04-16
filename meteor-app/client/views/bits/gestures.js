82e1804815c7d26f5c4ad8d99cfa1bc7329b5a4b
Steven J. Dale <lifeinchords@gmail.com>
2015-04-15 22:00:01 -0400

    fixed bug: long press gesture is meant to be on bits, not the map

--- meteor-app/client/views/bits/gestures.js
+++ meteor-app/client/views/bits/gestures.js
@@ -1,9 +1,10 @@
-Template.map.gestures({
-  'press .map': function (event, template) {
+Template.bit.gestures({
+  'press .bit': function (event, template) {
     /* `event` is the Hammer.js event object */
     /* `template` is the `Blaze.TemplateInstance` */
     /* `this` is the data context of the element in your template */
 
     sound.play('glue.mp3');
+    console.log('bit:long-press');
   }
 });
