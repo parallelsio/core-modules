55514fc256862c4b487e1dd3dd035a5c6f581b10
Steven J. Dale <lifeinchords@gmail.com>
2015-04-29 17:47:26 -0400

    wip - replaced sound samples with dynamically rendered sounds via Flocking js lib. Imported as client lib - no package

--- meteor-app/client/lib/sound.js
+++ meteor-app/client/lib/sound.js
@@ -2,12 +2,37 @@ Sound = {
 
   _folderPath: "sounds/",
 
-  play: function(soundFileName){
+  definition: {
+
+    synthDef: {
+        ugen: "flock.ugen.impulse",
+        freq: {
+            ugen: "flock.ugen.xLine",
+            start: 880,
+            end: 2,
+            duration: 4.0
+        },
+        mul: 0.25
+    }
+  },
+
+  play: function(definition){
 
     if (Meteor.settings.public.options.isSoundEnabled) {
-      var sound = new Howl({
-        urls: [ this._folderPath + soundFileName ]
-      }).play();
+ 
+      "use strict";
+
+      var enviro = flock.init();
+
+      // The Impulse ugen's frequency controlled by a descending xLine.
+      var synth = flock.synth(Sound.definition);
+              
+      enviro.play();
+      
     }
   }
+
+
+
+
 };
