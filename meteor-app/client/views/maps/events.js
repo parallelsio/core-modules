06ed4edd2a5c0449841d438e13faac06fc334349
Angel <angelcabo3@gmail.com>
2015-04-29 22:36:53 -0500

    add dropped event on map template to capture file drops

--- meteor-app/client/views/maps/events.js
+++ meteor-app/client/views/maps/events.js
@@ -1,3 +1,16 @@
+var eachFile = function(e, f) {
+  var evt = (e.originalEvent || e);
+
+  var files = evt.target.files;
+
+  if (!files || files.length == 0)
+    files = evt.dataTransfer ? evt.dataTransfer.files : [];
+
+  for (var i = 0; i < files.length; i++) {
+    f(files[i], i);
+  }
+};
+
 Parallels.Handlers.register('map.events', {
   'dblclick .map': function (event) {
     console.log("bit:text:create");
@@ -5,6 +18,11 @@ Parallels.Handlers.register('map.events', {
     if(event.target.classList.contains('map')) {
       Parallels.AppModes['create-bit'].enter(event);
     }
+  },
+  'dropped .map': function(event) {
+    eachFile(event, function (file) {
+      console.log('uploading file: ', file);
+    });
   }
 });
 
