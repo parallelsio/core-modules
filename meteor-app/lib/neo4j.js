3766719490a1d3cd645d4e4b345428d5117ccf38
Angel <angelcabo3@gmail.com>
2015-05-01 22:31:43 -0400

    officially remove ostrio:neo4jreactivity. Use neo4j driver with env variables for hosted graphdb

--- meteor-app/lib/neo4j.js
+++
@@ -1,6 +0,0 @@
-/* Allow client query execution */
-Meteor.neo4j.allowClientQuery = true;
-/* Custom URL to Neo4j should be here */
-Meteor.neo4j.connectionURL = process.env.NEO4J_URL;
-/* But deny all writing actions on client */
-Meteor.neo4j.set.deny(neo4j.rules.write);
--- meteor-app/server/methods.js
+++ meteor-app/server/methods.js
@@ -1,18 +1,28 @@
-Meteor.neo4j.methods({
-  'addBitToGraph': function(){
-    return 'CREATE (a:Bit {mongoId: {mongoId}})';
-  },
-  'removeBitFromGraph': function(){
-    return 'MATCH (a:Bit {mongoId: {mongoId}}) DELETE a';
-  }
-});
+var N4JDB = new Meteor.Neo4j(process.env.NEO4J_URL);
+
+function addBitToGraph (mongoId) {
+  N4JDB.query('CREATE (:Bit {mongoId:"' + mongoId + '"})', null, function (err) {
+    if (err) {
+      console.log('Neo4j addBitToGraph failed: ', mongoId, err);
+    }
+  });
+}
+
+function removeBitFromGraph (mongoId, cb) {
+  N4JDB.query('MATCH (a:Bit {mongoId:"' + mongoId + '"}) DELETE a', null, function (err) {
+    if (err) {
+      console.log('Neo4j removeBitFromGraph failed: ', mongoId, err);
+    }
+    cb(err, null);
+  });
+}
 
 Meteor.methods({
   'insertBit': function (attributes) {
     try {
       var mongoId = Bits.insert(attributes);
       console.log('methods:insertBit: created ', mongoId);
-      Meteor.call('addBitToGraph', {mongoId: mongoId});
+      addBitToGraph(mongoId);
       return mongoId;
     } catch(err) {
       console.log(err);
@@ -22,7 +32,8 @@ Meteor.methods({
     try {
       Bits.remove(id);
       console.log('methods:removeBit: ', id);
-      return Meteor.call('removeBitFromGraph', {mongoId: id});
+      var removeBitFromGraphPromise = Meteor.wrapAsync(removeBitFromGraph);
+      return removeBitFromGraphPromise(id);
     } catch(err) {
       console.log(err);
     }
