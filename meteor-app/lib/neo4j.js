3ad41ebad8ed89b7662c3066ee73abb94b4631c3
Angel <angelcabo3@gmail.com>
2015-05-01 22:05:02 -0400

    add neo4j url to heroku env variable

--- meteor-app/lib/neo4j.js
+++ meteor-app/lib/neo4j.js
@@ -1,6 +1,6 @@
 /* Allow client query execution */
 Meteor.neo4j.allowClientQuery = true;
 /* Custom URL to Neo4j should be here */
-Meteor.neo4j.connectionURL = Meteor.settings.neo4jUrl;
+Meteor.neo4j.connectionURL = process.env.NEO4J_URL;
 /* But deny all writing actions on client */
 Meteor.neo4j.set.deny(neo4j.rules.write);
