affd5c3d3e11adf7885e86f292e28259547c1929
Steven J. Dale <lifeinchords@gmail.com>
2015-05-19 17:05:32 +0200

    bug: dble click image bit shows form, which is meant only for text. Disabling bit:image edit

--- meteor-app/client/modes/edit.bit.js
+++ meteor-app/client/modes/edit.bit.js
@@ -1,11 +1,27 @@
 Parallels.AppModes['edit-bit'] = {
   enter: function (id) {
-    Session.set('currentMode', 'edit-bit');
-    Session.set('bitEditingId', id);
-    log.debug("bit:edit: " + Session.get('bitEditingId'));
+
+    var $bit = $("[data-id='" + id + "']");
+    var bitTemplate = Blaze.getView($bit[0]);
+    var bitData = Blaze.getData(bitTemplate);
+
+    // currently, we're only supporting edit for text type
+    if (bitData.type === "text") {
+      Session.set('currentMode', 'edit-bit');
+      Session.set('bitEditingId', id);
+      log.debug("bit:edit: " + Session.get('bitEditingId'));
+    }
+
+    else {
+      log.debug("bit:edit: bit not of type 'text'. Can't edit this yet."); 
+    }
+
   },
   exit: function () {
     Session.set('currentMode', null);
     Session.set('bitEditingId', null);
   }
 };
+
+
+
