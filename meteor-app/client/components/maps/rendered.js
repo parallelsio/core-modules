656f2283db13b4a44c0f5c276c69369f2af3fc67
Steven J. Dale <lifeinchords@gmail.com>
2015-05-21 11:13:13 +0200

    updating ploma js, which implements setStrokes. A refactoring pass at Ploma init code on map

--- meteor-app/client/components/maps/rendered.js
+++ meteor-app/client/components/maps/rendered.js
@@ -1,24 +1,13 @@
-Template.map.onRendered(function (){
-  
-  Parallels.Audio.player.play('fx-welcome-v1');
-
-  var mapTemplate = this;
-  var container = mapTemplate.find('.map');
-
-  // ***************** PLOMA *************************
-  var isDrawing = false;
-  var canvas = $("#sketch-bit")[0];
-  var plugin = $('#wtPlugin')[0];
-  
   // Ploma only does rendering
-  // doesnt care location + pressure data from
+  // doesnt care where location + pressure data come from
   // Ploma can be used with any tablet
-  var getEventPoint = function(e){
+  var getEventPoint = function(event, canvas, plugin){
+
     var point = {};
 
-    // account for offset
-    point.x = window.pageXOffset + e.clientX - canvas.offsetLeft;
-    point.y = window.pageYOffset + e.clientY - canvas.offsetTop;
+    // account for where the canvas sits, offset from 0,0
+    point.x = (window.pageXOffset + event.clientX) - canvas.offsetLeft;
+    point.y = (window.pageYOffset + event.clientY) - canvas.offsetTop;
 
     // fail gracefully if no pressure is detected (no tablet found)
     // tablet reports a range of 0 to 1
@@ -26,32 +15,48 @@ Template.map.onRendered(function (){
     return point;
   }
 
+
+var createPlomaCanvas = function(canvas){
+
+  // NAPAPI, necessary for interfacing with Wacom tablet
+  var plugin = document.getElementById('wtPlugin'); 
+  var isDrawing = false;
   var ploma = new Ploma(canvas);
+
   ploma.clear();
 
   // begin a stroke at the mouse down point
-  canvas.onmousedown = function(e) {
+  canvas.onmousedown = function(event) {
     isDrawing = true;
-    var p = getEventPoint(e);
-    ploma.beginStroke(p.x, p.y, p.p);
+    var point = getEventPoint(event, canvas, plugin);
+    ploma.beginStroke(point.x, point.y, point.p);
   }
 
   // extend the stroke at the mouse move point
-  canvas.onmousemove = function(e) {
+  canvas.onmousemove = function(event) {
     if (!isDrawing) return;
-    var p = getEventPoint(e);
-    ploma.extendStroke(p.x, p.y, p.p);
+    var point = getEventPoint(event, canvas, plugin);
+    ploma.extendStroke(point.x, point.y, point.p);
   }
 
   // end the stroke at the mouse up point
-  canvas.onmouseup = function(e) {
+  canvas.onmouseup = function(event) {
     isDrawing = false;
-    var p = getEventPoint(e);
-    ploma.endStroke(p.x, p.y, p.p);
+    var point = getEventPoint(event, canvas, plugin);
+    ploma.endStroke(point.x, point.y, point.p);
   }
+}
 
-  // ***************** PLOMA *************************
 
+
+Template.map.onRendered(function (){
+  
+  Parallels.Audio.player.play('fx-welcome-v1');
+
+  var mapTemplate = this;
+  var container = mapTemplate.find('.map');
+  
+  createPlomaCanvas(document.getElementById("sketch-bit"));
 
   container._uihooks = {
 
