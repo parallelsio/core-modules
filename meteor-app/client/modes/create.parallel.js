230031362291c7afbaade798680ad9d451a1c313
Steven J. Dale <lifeinchords@gmail.com>
2015-05-26 04:26:02 +0200

    * resetting vars after parallel mode exit * moving line to start in bit top left corner * adding back dashed stroke

--- meteor-app/client/modes/create.parallel.js
+++ meteor-app/client/modes/create.parallel.js
@@ -1,11 +1,12 @@
-var timeline, $bitOrigin;
-
 function getRandomColor() {
   return 'rgb('
     + Math.floor(Math.random() * 255) + ','
     + Math.floor(Math.random() * 255) + ','
     + Math.floor(Math.random() * 255) + ')';
 }
+var timeline, $bitOrigin;
+var line, updatedLine, lineContainer, params, two, mouse;
+
 
 /*
  TODO:
@@ -14,6 +15,7 @@ function getRandomColor() {
  * is timeline.kill() the best way to gracefully end heartbeat animation on escape?
 
  */
+
 Parallels.AppModes['create-parallel'] = {
   enter: function () {
     Session.set('currentMode', 'create-parallel');
@@ -33,83 +35,77 @@ Parallels.AppModes['create-parallel'] = {
 
       log.debug("ready for creating parallel. starting at bit: " + bitParallelCreateOriginId);
 
-      // abstract out into reusable mode concept.
+      // TODO: abstract out into reusable mode concept.
       // here, it might be : enterMode.createParallel()
       $(".map").addClass('mode--create-parallel'); // visually demonstrate we're in connecting mode
 
       // TODO: animate
-      // $bitOrigin.addClass('dashed-stroke');
+      $bitOrigin.addClass('dashed-stroke');
 
       // TODO:
       // disable events
       // try bg overlay over other bits too?
       // disable scrolling
 
-      // TODO: make helper on Bit obj
-      // var getBitCenterPoint function(){
-
-      // }
-      
-
       // TODO : get working with Hammer
 
       // set up an SVG container via two.js container to draw the line stroke
-      var element = document.createElement('div');
-      $(element)
+      lineContainer = document.createElement('div');
+      $(lineContainer)
         .addClass("create-parallel--line-container")
         .height( 5000 )
         .width( 5000)
         .prependTo(".map");
 
-      var params = { 
+      params = { 
         fullscreen: true,
         autostart: true
       };
-      var two = new Two(params).appendTo(element);
-      var mouse = new Two.Vector();
+
+      two = new Two(params).appendTo(lineContainer);
+      mouse = new Two.Vector();
 
       // OQ: should we use Meteor binding? 
       // https://stackoverflow.com/questions/21486667/meteor-js-how-should-i-bind-events-to-the-window-in-meteor
-       $(window).on('mousemove', function(event){
-          log.debug("mouse event.page_: ", event.pageX, event.pageY);
-          mouse.x = window.pageXOffset + event.clientX;
-          mouse.y = window.pageYOffset + event.clientY
-        });
+      $(window).on('mousemove', function(event){
+        mouse.x = window.pageXOffset + event.clientX;
+        mouse.y = window.pageYOffset + event.clientY
+      });
 
       // TODO: originate line from center point of bit, so it looks right
       // as person moves mouse around: var centerCoords = getCenterPointOfBit()
       // destination vector is will be overridden on first
       // update by mouse position and won't be visible.
-      var line = two
-        .makeLine( 
-          bitData.position.x,
-          bitData.position.y,
-          bitData.position.x, 
-          bitData.position.y
-        )
-        line.linewidth = 15;
-        line.cap = line.join = 'round';
-      
+      line = two.makeLine( 
+              bitData.position.x,
+              bitData.position.y,
+              bitData.position.x, 
+              bitData.position.y
+      );
+      line.linewidth = 15;
+      line.cap = line.join = 'round';
+    
       two.bind('update', function(frameCount) {
         /*  OQ: why doesnt this work?
-            var endAnchor = new Two.Anchor(mouse.x, mouse.y);
-            line.vertices[1] = endAnchor;
+              var endAnchor = new Two.Anchor(mouse.x, mouse.y);
+              line.vertices[1] = endAnchor;
         */
 
-        // OQ: is this the best strategy for this?
+        // OQ: is clearing the canvas the best strategy for this?
+        // in terms of painting performance?
         two.clear();
 
-        var newLine = two
-          .makeLine( 
-            bitData.position.x,
-            bitData.position.y,
-            mouse.x, 
-            mouse.y
-          )
-        newLine.linewidth = 15;
-        newLine.cap = line.join = 'round';
-        
+        updatedLine = two.makeLine( 
+              bitData.position.x,
+              bitData.position.y,
+              mouse.x, 
+              mouse.y
+        );
 
+        // TODO: longer the distance, thinner the linewidth
+        updatedLine.linewidth = 15;
+        updatedLine.cap = updatedLine.join = 'round';
+        
         // TODO: depending on direction on direction parallel is pointing,
         // addClass(left);
 
@@ -125,7 +121,6 @@ Parallels.AppModes['create-parallel'] = {
         // Session.set('bitParallelCreateOriginId', null);
 
         // $(this).unbind();
-
       };
 
       timeline = new TimelineMax({
@@ -139,20 +134,11 @@ Parallels.AppModes['create-parallel'] = {
         // play heartbeat animation
         .to($bitOrigin, 0.50, { scale: 1.02, ease:Expo.easeOut } )
         .to($bitOrigin, 0.5, { scale: 1, ease:Expo.easeOut } );
-
-
-      // TODO: only enable if none others are going
-
-      // TODO: move to map? merge map.js + app.js?
-
-      // $(this).mousemove( function(event) {
-      //   log.debug("mouse event.page_: ", event.pageX, event.pageY);
-      // });
     }
   },
+
   exit: function () {
     Session.set('currentMode', null);
-
     log.debug('escape key, inside create parallel, exiting mode');
 
     var isCreatingParallel = Session.get('isCreatingParallel');
@@ -163,7 +149,8 @@ Parallels.AppModes['create-parallel'] = {
       Session.set('bitParallelCreateOriginId', null);
 
       $(".map").removeClass('mode--create-parallel');
-      // $bitOrigin.removeClass('dashed-stroke');
+      
+      $bitOrigin.removeClass('dashed-stroke');
       $('.create-parallel--line-container').remove();
 
       // stop heartbeat animation
@@ -172,6 +159,8 @@ Parallels.AppModes['create-parallel'] = {
       // TODO: remove all two instances
 
       $(window).off("mousemove");
+      lineContainer, params, two, mouse, updatedLine, line = null;
+
 
       log.debug('parallel:create: exit mode');
     }
