31ac89c03ad74453528f9a4fe449ab5ee6775844
Angel <angelcabo3@gmail.com>
2015-05-31 16:25:51 -0400

    fix bug introduced in create bit mode

--- meteor-app/client/modes/create.bit.js
+++ meteor-app/client/modes/create.bit.js
@@ -3,15 +3,16 @@ Parallels.AppModes['create-bit'] = {
   enter: function (event) {
     log.debug("mode:create-bit:enter");
 
+    var center = Utilities.getViewportCenter();
+
     Session.set('currentMode', 'create-bit');
     Session.set('createTextBit', {
-      canvasId: '1', 
-      type: 'text', 
-      color: 'white', 
-      content: '', 
+      canvasId: '1',
+      type: 'text',
+      color: 'white',
+      content: '',
       position: {
-        var center = Utilities.getViewportCenter();
-        x: center.x
+        x: center.x,
         y: center.y
       }
     });
