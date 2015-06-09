8b3eb12ec9a3324a12ba84eadf7ceb2aba9fdd75
Steven J. Dale <lifeinchords@gmail.com>
2015-06-10 00:24:15 +0200

    * set up Browserify, allowing us use of npm dependencies for client side use * adding Teoria JS lib, for music theory objects: note, chords, scales, intervals

---
+++ meteor-app/packages.browserify.js
@@ -0,0 +1,4 @@
+/*  NOTE: Due to Meteor Issue #3985: https://github.com/meteor/meteor/issues/3985
+    we must put something before the extension, like: app.browserify.js.
+    */
+teoria = require('teoria');
