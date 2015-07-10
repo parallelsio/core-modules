efddb45473a267b1868ad3fdbbbb8c3c48b4b6aa
Steven Dale <lifeinchords@gmail.com>
2015-07-10 19:35:06 +0200

    bug: removing duplicate session set

--- meteor-app/client/main.js
+++ meteor-app/client/main.js
@@ -10,9 +10,6 @@ Meteor.startup(function(){
     }
   });
 
-  // TODO: make Session bootup/init?
-  Session.set('createTextBit', null);
-
   Tracker.autorun(function() {
     console.log(Bits.find().count() + ' bits, via Tracker:autorun');
   });
@@ -21,6 +18,5 @@ Meteor.startup(function(){
     console.log('Session:bitHoveringId is now: ', Session.get("bitHoveringId"), ', via Tracker:autorun');
   });
 
-  // OQ: why is this reset twice? it's set above: line 9?
   Session.set('createTextBit', null);
 });
