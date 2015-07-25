344c71b31d93d1e52704ab2efb2aa1d93fbbee8b
Angel <angelcabo3@gmail.com>
2015-07-25 12:52:08 -0400

    version lock the build deps for build consistency

--- test-scripts/bin/run_meteor_package_units.sh
+++ test-scripts/bin/run_meteor_package_units.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# On Mac OS X GNU grep is ggrep.
-if hash ggrep 2>/dev/null; then
-  GREP='ggrep'
-else
-  GREP='grep'
-fi
-
 SPECIFIED_PACKAGES="$1"
 if [ -z "${SPECIFIED_PACKAGES}" ]; then
-  PACKAGES=( $(find meteor-app/packages -type d | $GREP -v .npm | $GREP -i '\/test$' | $GREP -Po 'meteor-app/packages/\K.*(?=/)' | sort -u) )
+  PACKAGES=( $(find meteor-app/packages -type d | grep -v .npm | grep -i '\/test$' | perl -nle 'print $& if m{meteor-app/packages/\K.*(?=/)}' | sort -u) )
 else
   PACKAGES=("$SPECIFIED_PACKAGES")
 fi
