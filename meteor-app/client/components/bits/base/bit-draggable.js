3e64f44bd56948426d88ea27b4aba7b14489837c
steven dale <lifeinchords@gmail.com>
2016-09-03 16:53:54 -0400

    bit:drag - first pass at neighbor snapping

--- meteor-app/client/components/bits/base/bit-draggable.js
+++ meteor-app/client/components/bits/base/bit-draggable.js
@@ -1,6 +1,8 @@
 makeBitDraggable = function makeBitDraggable($bitElement, $dragHandle){
 
   var timeline = new TimelineMax();
+  var targets = $(".bit");
+  var overlapThreshold = "0%"; 
 
   // // Needs to happen after position set, or else positions
   // // via manual transforms get overwritten by Draggable
@@ -10,6 +12,9 @@ makeBitDraggable = function makeBitDraggable($bitElement, $dragHandle){
     throwProps:false,
     zIndexBoost:false,
     autoScroll: 1,
+    // bounds:,
+    // edgeResistance:0.65,
+    // type:"x,y", // default?
 
     onPress: function(event){
     
@@ -28,6 +33,12 @@ makeBitDraggable = function makeBitDraggable($bitElement, $dragHandle){
         .to($bitElement.find('.bit__drag-handle'), 0.1, { scale: 1.5, opacity: "0.1", ease: Expo.easeOut }), "-=0.10";
     },
 
+    onRelease: function(event){
+      timeline
+        .to($bitElement, 0.1, { scale: 1, boxShadow: "0", ease: Expo.easeOut })
+        .to($bitElement.find('.bit__drag-handle'), 0.1, { scale: 1, opacity: "1", ease: Expo.easeOut }), "-=0.10";
+    },
+
     onDragStart:function(event){
 
       // var x = this.endX;
@@ -41,34 +52,54 @@ makeBitDraggable = function makeBitDraggable($bitElement, $dragHandle){
 
       // TODO: ensure this happens only when in Draggable and mouse is held down
       // and not on regular taps/clicks of bit
-      timeline
-        .to($bitElement, 0.20, {
-          scale: 1.05,
-          boxShadow: "rgba(0, 0, 0, 0.2) 0 16px 32px 0",
-          ease: Expo.easeOut
-        });
+      // timeline
+      //   .to($bitElement, 0.20, {
+      //     scale: 1.05,
+      //     boxShadow: "rgba(0, 0, 0, 0.2) 0 16px 32px 0",
+      //     ease: Expo.easeOut
+      //   });
+
         // .to($bitElement.find('.bit__drag-handle'), 0.1, { scale: 1.5, opacity: "0.1", ease: Expo.easeOut })
 
+        $bitElement.addClass('grabbing');
     },
 
-
-    onRelease: function(event){
-      timeline
-        .to($bitElement, 0.1, { scale: 1, boxShadow: "0", ease: Expo.easeOut })
-        .to($bitElement.find('.bit__drag-handle'), 0.1, { scale: 1, opacity: "1", ease: Expo.easeOut }), "-=0.10";
+    // TODO: restrict search to only what's visible in viewport, via Verge?
+    onDrag: function(e) {
+      for(var i = 0; i < targets.length; i++){
+        if (this.hitTest(targets[i], overlapThreshold)) {
+           $(targets[i]).addClass("bit--near");
+         } else {
+           $(targets[i]).removeClass("bit--near");
+         }
+      }
     },
 
-
-    onDragStart:function(event){
-      // var x = this.endX;
-      // var y = this.endY;
-      // Parallels.log.debug(event.type, " : dragStart: ", x, " : ", y, " : ", this.getDirection("start"), " : ");
-      Parallels.Audio.player.play('fx-cinq-drop');
-    },
 
     onDragEnd:function( event ) {
+
       var x = this.endX;
       var y = this.endY;
+
+      // snap to nearest bit
+      // via http://greensock.com/forums/topic/9265-draggable-snapping-to-specific-points-with-sensitivity/
+
+      // TODO: only pick one, if multiple
+      // snap to axis depending on if above/below center line, left/r from cross line
+      // var snapMade = false;
+      for(var i = 0; i < targets.length;i++){
+        if(this.hitTest(targets[i], overlapThreshold)){
+          var rect = $(targets[i])[0].getClientRects()[0];
+          // snapMade = true;
+          x = rect.left + rect.width;
+          y = rect.top + rect.height;
+        }
+      }
+
+      console.log('x:y: ', x, ':', y);
+      TweenLite.to($bitElement, 0.1, { x: x, y: y, ease: Circ.easeOut });
+
+
       var mongoId = this.target.dataset.id;
 
       timeline.to(
@@ -96,7 +127,13 @@ makeBitDraggable = function makeBitDraggable($bitElement, $dragHandle){
       timeline
         .to($bitElement, 0.1, { scale: 1, boxShadow: "0", ease: Expo.easeOut })
         .to($bitElement.find('.bit__drag-handle'), 0.1, { scale: 1, opacity: "1", ease: Expo.easeOut })
+
+      $('.bit--near').removeClass("bit--near");
+      $bitElement.addClass('grabbing');
+
     }
+
+
   });
 
   return draggable;
